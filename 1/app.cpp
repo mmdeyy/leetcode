aa00fc6180123fcdbcd436720751f375a81ff087
unknown <wydewy@126.com>
2017-09-17 21:47:33 +0800

    move some code

--- 1/app.cpp
+++ 1/app.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> Solution::twoSum(vector<int>& nums, int target)
 {
-	 vector<int> result = new vector();
+	 vector<int> result();
 	 int n = nums.size();
 	  for(int i = 0,j = n-1;(i<n)&&(j>=0);){
            if(nums[j]+nums[i]>target){
