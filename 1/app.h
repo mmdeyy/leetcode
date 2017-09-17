6dfad241705b4b0841303a092f0b7a306d86acc4
unknown <wydewy@126.com>
2017-09-17 12:17:39 +0800

    move some code

--- 1/app.h
+++ 1/app.h
@@ -1,6 +1,8 @@
 #ifndef  APP_H              
-#define  APP_H              
- 
+#define  APP_H 
+             
+#include<vector>
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target);
--- 1/main.cpp
+++ 1/main.cpp
@@ -1,5 +1,4 @@
-#include<iostream>  
-#include<vector>
+#include<iostream> 
 #include "app.h"
 using namespace std;
 int main()
